8f36ef722b1cba6d9ae2c488124efa6508db01d4
Brian Lee <brianjleeofcl@gmail.com>
2017-07-20 12:59:35 -0700

    Add server side validation for message requests

--- server/src/v1/routes/email.js
+++ server/src/v1/routes/email.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const boom = require('boom')
-
-const nodemailer = require('nodemailer')
-
-const util = require('util')
+const boom = require('boom');
+const isEmail = require('isemail');
+const nodemailer = require('nodemailer');
 
 router.post('/send', (req, res, next) => {
-  const data = req.body
+  const {email, message, name} = req.body
+
+  if (email.length === 0 || name.length === 0) return next(boom.badRequest('Empty Fields'))
+
+  const validEmail = isEmail.validate(email)
+  if (!validEmail) return next(boom.badRequest('Invalid Email'))
   
   const transporter = nodemailer.createTransport({
     host: 'mail.name.com',
@@ -20,16 +23,16 @@ router.post('/send', (req, res, next) => {
   })
 
   const mailOption = {
-    from: req.body.email,
+    from: email,
     to: process.env.EMAIL_USER,
-    text: req.body.message,
-    subject: `Message from ${req.body.name}`
+    text: message,
+    subject: `Message from ${name}`
   }
 
   transporter.sendMail(mailOption, (error, info) => {
     if (error) {
       console.error(error)
-      next(boom.badImplementation(error))
+      return next(boom.badImplementation(error))
     }
     else {
       res.send('Successfully sent')
