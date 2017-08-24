b414e66c4e3efafbd2f5a7f267bd15754ffb6329
Brian Lee <brianjleeofcl@gmail.com>
2017-08-23 17:57:42 -0700

    Retrieve img urls from meta tag data

--- server/v1/routes/projects.js
+++ server/v1/routes/projects.js
@@ -6,6 +6,7 @@ const boom = require('boom')
 
 router.get('/', (req, res, next) => {
   let img;
+  let projectsJSON;
 
   axios.get('https://gist.githubusercontent.com/brianjleeofcl/3e195fe45a71373f33ba2de1e022893b/raw/projects.txt').then(({data}) => {
     const projects = data.replace(/\$/g, 'brianjleeofcl').split('\n').map(str => str.split(' '));
@@ -21,7 +22,9 @@ router.get('/', (req, res, next) => {
 
     return Promise.all(promises)
   }).then(results => {
-    const projects = results.map(({data}, i) => {
+    const imgPromises = [];
+
+    projectsJSON = results.map(({data}, i) => {
       const { html_url, homepage, name, description } = data
       const title = titlify(name);
       const url = {
@@ -29,15 +32,36 @@ router.get('/', (req, res, next) => {
         site: homepage
       }
       const res = { title, desc: description, url };
-      if (img[i]) res.img = img[i];
+
+      if (img[i]) imgPromises[i] = img[i];
+      else imgPromises[i] = getMetaTagPromise(homepage);
+
       return res
     });
-    res.set('Access-Control-Allow-Origin', '*')
-    res.send(projects);
+
+    return Promise.all(imgPromises)
+  }).then(imgURLs => {
+    const resultJSON = projectsJSON.map((obj, i) => {
+      if (imgURLs[i] !== null) obj.img = imgURLs[i];
+      return obj;
+    });
+
+    res.send(resultJSON);
   }).catch(err => {
     console.error(err)
     res.next(boom.badImplementation(err.message, err))
   });
 });
+
+function getMetaTagPromise(url) {
+  return axios.get(url).then(({data}) => {
+    const metaTag = data.match(new RegExp('<meta property="og:image" content="(.+?)"'))[1];
+    return metaTag;
+  }).catch(() => {
+    return null;
+  })
+}
+
+
 
 module.exports = router;
